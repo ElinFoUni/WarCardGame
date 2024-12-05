c572482e8142fee619efdb56a9d925fa5fddf04f
ElinFoUni <3ggapp24.tvinel@folkuniversitetet.nu>
2024-12-05 11:09:10 +0100

    Created a player object in Main. Changed folder structure a bit.

--- app/src/main/java/com/example/war/classes/Card.kt
+++ app/src/main/java/com/example/war/classes/Card.kt
@@ -1,4 +1,5 @@
 package com.example.war.classes
 
-class Card {
+data class Card(val rank: Int, val suit: String) {
+
 }
--- app/src/main/java/com/example/war/classes/Deck.kt
+++ app/src/main/java/com/example/war/classes/Deck.kt
@@ -1,5 +1,34 @@
 package com.example.war.classes
 
 class Deck {
+    //make a changable list of cards
+    val cards = mutableListOf<Card>()
+
+    init {
+        populateDeck()
+    }
+
+    fun populateDeck() {
+        val suits = listOf("Clover", "Diamonds", "Hearts", "Spades")
+        for (suit in suits) {
+            for (rank in 2..14) {
+                cards.add(Card(rank, suit))
+            }
+        }
+    }
+
+    //shuffles the deck
+    fun shuffleDeck() {
+        cards.shuffle()
+    }
+
+    fun dealCard(): Card {
+        return cards.removeAt(0)
+    }
+
+    // can be used to determine winner/show how many cards left in deck
+    fun getCardCount(): Int {
+        return cards.size
+    }
 
 }
