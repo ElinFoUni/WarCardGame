e5ff4303daf8c155a562acba66cbbe3b2e79e8ba
ElinFoUni <3ggapp24.tvinel@folkuniversitetet.nu>
2024-12-05 06:47:16 +0100

    Player can enter their name

--- app/src/main/java/Classes/Game.kt
+++ app/src/main/java/Classes/Game.kt
@@ -1,4 +1,22 @@
 package Classes
 
 class Game {
+
+    fun winBattleState() {
+
+    }
+
+    fun loseBattleState() {
+
+    }
+
+    //(Simple version - cannot actually use the "war" mechanic for now)
+    fun drawBattleState() {
+
+    }
+
+    /*fun war() {
+
+    }*/
+
 }
