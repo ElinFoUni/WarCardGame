9eb8d99899828e636e1437a6b76396015f80f1c2
ElinFoUni <3ggapp24.tvinel@folkuniversitetet.nu>
2024-12-05 12:01:33 +0100

    Updated some classes

--- app/src/main/java/com/example/war/classes/Card.kt
+++ app/src/main/java/com/example/war/classes/Card.kt
@@ -2,4 +2,15 @@ package com.example.war.classes
 
 data class Card(val rank: Int, val suit: String) {
 
+    //set cards to images in drawables folder:
+    fun getCardImage(): String {
+        val suitMap = mapOf(
+            "hearts" to "hearts",
+            "diamonds" to "diamonds",
+            "spades" to "spades",
+            "clover" to "clover"
+        )
+        return "${rank}_${suitMap[suit]}"
+    }
+
 }
--- app/src/main/java/com/example/war/classes/Deck.kt
+++ app/src/main/java/com/example/war/classes/Deck.kt
@@ -1,30 +1,32 @@
 package com.example.war.classes
 
 class Deck {
-    //make a changable list of cards
-    val cards = mutableListOf<Card>()
+    private val suits = listOf("hearts", "diamonds", "spades", "clover")
+    private val ranks = (2..14) //14 is Ace, 13 is King etc
 
+    //makes a changable list of cards
+    private val cards = mutableListOf<Card>()
+
+    //adds cards to the deck and shuffles
     init {
-        populateDeck()
-    }
-
-    fun populateDeck() {
-        val suits = listOf("Clover", "Diamonds", "Hearts", "Spades")
         for (suit in suits) {
-            for (rank in 2..14) {
+            for (rank in ranks) {
                 cards.add(Card(rank, suit))
             }
         }
-    }
-
-    //shuffles the deck
-    fun shuffleDeck() {
         cards.shuffle()
     }
 
-    fun dealCard(): Card {
-        return cards.removeAt(0)
+    fun dealCard(): Card? {
+        return if (cards.isNotEmpty()) cards.removeAt(0) else null
     }
+
+    //Simple version of the draw, should be replaced by a "war" function in actual game.
+    // Doesn't work atm
+    // fun addCardsToBottom(drawedCards: List<Card>) {
+    //    cards.addAll(newCards)
+    //}
+
 
     // can be used to determine winner/show how many cards left in deck
     fun getCardCount(): Int {
